6ead484749a3d1408af1aa07bfd188f18a11d0d3
Halldór Rúnar <halldorv18@ru.is>
2018-10-15 21:40:16 +0000

    Second commit

--- skilaverkefni/ticTacToe.py
+++ skilaverkefni/ticTacToe.py
@@ -9,11 +9,37 @@ def make_list(size):
         return_list.append(THE_LIST)
     return return_list
 
-def user_input():
+def user_input(next_move):
+    while True:
+        if next_move == "X":
+            user_choice = int(input("X position: "))
+            break
+        elif next_move == "O":
+            user_choice = int(input("O position: "))
+            break
+        else:
+            print("Illegal move!")
+            continue
+    return user_choice
+    
+    
+def change_board(user_choice, x_or_o, board_list):
+    for lines in board_list:
+        for index, positions in enumerate(lines):
+            if user_choice == positions:
+                lines.remove(positions)
+                lines.insert(index, x_or_o)
+    return board_list
 
+                
+
+
+next_move = "X"
 size_of_game = int(input("Input dimension of the board:"))
 board_list = make_list(size_of_game)
+user_choice = user_input(next_move)
+board_list = change_board(user_choice, next_move, board_list)
+print(board_list)
 
-for line in board_list:
 
-    print(line)
+
