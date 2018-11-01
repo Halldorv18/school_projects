ff9f31d0077df985818d64c4086c79632356c15e
Halldór Rúnar <halldorv18@ru.is>
2018-11-01 14:32:16 +0000

    Predator-Prey first commit

---
+++ assignments/biggies/predator_prey.py
@@ -0,0 +1,13 @@
+
+    tenerife = Island(4,)
+    monkey = Animal(tenerife, 0, 3, "M")
+    
+    tenerife.set_animal_on_island(monkey)
+    print(tenerife)
+    monkey.move()
+    print(tenerife)
+   
+
+    
+
+main()
